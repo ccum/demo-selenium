e2e1197e15187447ec3e72e58efb54a4bcc72654
Cesar Cueva Mendoza <cesar.cueva@bbva.com>
2019-01-23 09:29:41 -0500

    cambios

--- src/Demo.java
+++ src/Demo.java
@@ -7,11 +7,31 @@ public class Demo {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
+		facebook1();
+		//salesforce();
+
+	}
+
+	private static void salesforce() {
+		System.setProperty("webdriver.chrome.driver", "/home/cesarcueva/Documents/web-drivers/chromedriver_linux64/chromedriver");
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://login.salesforce.com");
+		driver.findElement(By.id("username")).sendKeys("hello");
+		driver.findElement(By.name("pw")).sendKeys("hello");
+		//driver.findElement(By.className("button r4 wide primary")).click(); ---> error
+		driver.findElement(By.xpath("//*[@id=\'Login\']")).click();
+	}
+
+	private static void facebook1() {
 		System.setProperty("webdriver.chrome.driver", "/home/cesarcueva/Documents/web-drivers/chromedriver_linux64/chromedriver");
 		WebDriver driver = new ChromeDriver();
 		driver.get("http://facebook.com");
-		driver.findElement(By.id("email")).sendKeys("mi email");
-		
+		//driver.findElement(By.id("email")).sendKeys("mi email");
+		driver.findElement(By.cssSelector("#email")).sendKeys("mi email");
+		//driver.findElement(By.name("pass")).sendKeys("123456");
+		driver.findElement(By.xpath("//*[@id=\'pass\']")).sendKeys("123456");
+		//driver.findElement(By.linkText("Forgot account?")).click();
+		driver.findElement(By.xpath("//*[@id=\'login_form\']/table/tbody/tr[3]/td[2]/div/a")).click();
 	}
 
 	private static void demo01() {
